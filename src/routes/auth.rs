c9d89dc9090f1fc9338af30d86cf86645a18d76e
z7pz <sdfygamer@gmail.com>
2024-02-09 00:57:33 +0300

    fix: add some todo tag

--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -70,7 +70,7 @@ async fn me(State(state): State<AppState>, Json(m): Json<Me>) -> impl IntoRespon
 }
 pub fn routes() -> Router<AppState> {
     Router::new()
-        .route("/all", get(all))
+        .route("/all", get(all)) // TODO: remove on production
         .route("/me", get(me))
         .route("/register", post(register))
         .route("/login", post(login))
